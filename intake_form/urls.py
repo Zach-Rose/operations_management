2ec84ce5ce18b0a6819195e9c7cfd81618b8a25f
Zach Rose <me@zacheryrose.com>
2025-01-05 16:28:34 -0500

    some fixes more

--- intake_form/urls.py
+++ intake_form/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),  # Add this line
     path('submit/', views.submit_process_form, name='submit_process_form'),
     path('success/', views.success_page, name='success_page'),
 ]
