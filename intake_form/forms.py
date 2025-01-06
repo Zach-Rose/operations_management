5706234c1578d9b2b04526f4737f5b4ca9319b95
Zach Rose <me@zacheryrose.com>
2025-01-06 12:42:09 -0500

    adding additional logging and helper text part deux

--- intake_form/forms.py
+++ intake_form/forms.py
@@ -10,18 +10,34 @@ class ContributorForm(forms.ModelForm):
 
 class ProcessStepForm(forms.ModelForm):
     contributors = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'contributor-input'}),
+        widget=forms.TextInput(attrs={
+            'class': 'contributor-input',
+            'autocomplete': 'off'
+        }),
         help_text='Separate each contributor by a comma and space (e.g., John Doe, Jane Smith)'
     )
-    duration_value = forms.IntegerField()
-    duration_unit = forms.ChoiceField(choices=[('seconds', 'Seconds'), ('minutes', 'Minutes'), ('days', 'Days')])
+    duration_value = forms.IntegerField(
+        widget=forms.NumberInput(attrs={'autocomplete': 'off'})
+    )
+    duration_unit = forms.ChoiceField(
+        choices=[('seconds', 'Seconds'), ('minutes', 'Minutes'), ('days', 'Days')],
+        widget=forms.Select(attrs={'autocomplete': 'off'})
+    )
 
     class Meta:
         model = ProcessStep
         fields = ['name', 'duration_value', 'duration_unit', 'contributors']
+        widgets = {
+            'name': forms.TextInput(attrs={'autocomplete': 'off'})
+        }
 
 
 class ProcessForm(forms.ModelForm):
     class Meta:
         model = Process
         fields = ['name', 'description', 'ideal_duration']
+        widgets = {
+            'name': forms.TextInput(attrs={'autocomplete': 'off'}),
+            'description': forms.Textarea(attrs={'autocomplete': 'off'}),
+            'ideal_duration': forms.NumberInput(attrs={'autocomplete': 'off'})
+        }
